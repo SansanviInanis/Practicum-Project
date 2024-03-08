f2f91d8378247f93a11904bbec7163b4ebdef824
SansanviInanis <159854416+SansanviInanis@users.noreply.github.com>
2024-03-07 22:39:12 -0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,12 @@
+// Function to handle "View Timesheet" click event
+function onViewTimesheetClick(timesheetID) {
+    // Determine if the user is an admin or employee
+    // If admin, enable editing of additional fields
+    // If employee, make all fields read-only except description
+}
+
+// Function to calculate "Time Worked"
+function calculateTimeWorked(start_time, end_time, hourly_slots) {
+    // Calculate total time worked based on shift start/end times and hourly slots
+    // Return the calculated time in hours and minutes
+}
